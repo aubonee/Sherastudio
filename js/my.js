1bf052b0e841faa5371a392f6bcd247b1ddb624a
Rahidapriya <rahidapriyacse@gmail.com>
2022-01-04 20:01:57 +0600

    first commit

---
+++ js/my.js
@@ -0,0 +1,11 @@
+$('.responsive').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow:null,
+    nextArrow:null,
+    autoplaySpeed: 700,
+    
+   /* prevArrow:".leftbtn",
+    nextArrow:".rightbtn",*/
+  });
